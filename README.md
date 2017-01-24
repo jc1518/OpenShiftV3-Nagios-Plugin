5f90d3b4027ac069b7884b659654a00bbfef80bf
Jackie Chen <support@jackiechen.org>
2017-01-24 15:03:18 +1100

    openshift persistent storage usage monitoring

---
+++ README.md
@@ -0,0 +1,35 @@
+# OpenShift V3 Nagios Plugins
+
+## Overview
+
+By the time of writing, OpenShift V3 comes with poor monitoring capabilities. The build-in monitoring only checks the metrics of Memory/CPU/Network, and it does not even support alerting! And the lowest granular level only down to last hour. So you have to build your own monitoring if you want to keep close eyes on your services running on OpenShift.  
+
+This project aims to develop some Nagios plugins for OpenShift V3. Here are the plugins that are available now:
+
+- [x] Persisent storage usage monitor
+
+## Installations  
+
+1. Install OpenShift client (oc) on your Nagios box.   
+2. Create a monitoring account, then get the token. 
+```bash
+echo '{
+  "apiVersion": "v1",
+  "kind": "ServiceAccount",
+  "metadata": {
+    "name": "nagios"
+  }
+}' > nagiosSA.json
+oc create -f nagiosSA.json
+oc adm policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:nagios
+oc get secrets
+oc describe secret nagios-token-xxxx
+```
+3. Copy the plugin file into the Nagios libexec folder, typically it is /usr/local/nagios/libexec/. Then replace thisisyourtoken... with the token you got above in 'your_token=thisisyourtokenpleasekeepitsecureyour'.
+
+## Usage
+** Persisent storage usage monitor
+```bash
+/usr/local/nagios/libexec/openshift_pv_check.sh project_name
+```
+![pv](files/pv.png)
---
+++ openshift_pv_check.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Monitor Openshift V3 persisent storage usage
+
+project=$1
+your_token=thisisyourtokenpleasekeepitsecure
+
+if [ -z $1 ]; then
+        echo "Project name is required"
+        exit 3
+fi
+
+export KUBECONFIG=/usr/local/nagios/libexec/kubeconfig
+message_text="Disk Usage:"
+message_exit=0
+
+date > /tmp/ospv.log
+/usr/bin/oc login https://api.newscorpau01.openshift.com --token=$your_token >> /tmp/ospv.log 2>&1
+/usr/bin/oc project $project >> /tmp/ospv.log 2>&1
+pods=$(/usr/bin/oc get pods | grep -v -e build -e NAME | awk '{print $1}')
+for pod in $pods
+do
+                volumes=$(/usr/bin/oc volume pod/$pod | grep 'mounted at' | grep -v 'kubernetes.io' | awk '{print $3}')
+                for volume in $volumes
+                do
+                        usage=$(/usr/bin/oc exec $pod -- df -h $volume | awk '{print $5}' | tail -1 | cut -d'%' -f1)
+                        if [ $usage -gt 80 ] && [ $usage -lt 90 ]; then
+                                message_temp_exit=1
+                                message_temp_text="Warning - $volume is $usage% full!"
+                        elif [ $usage -ge 90 ]; then
+                                message_temp_exit=100
+                                message_temp_text="Critical - $volume is $usage% full!"
+                        else
+                                message_temp_exit=0
+                                message_temp_text="OK - $volume is $usage% full."
+                        fi
+                        let message_exit=${message_exit}+${message_temp_exit}
+                        message_text=${message_text}' '${message_temp_text}
+                done
+done
+
+echo "$message_text"
+if [ $message_exit -ge 100 ]; then
+        exit 2
+elif [ $message_exit -ge 1 ]; then
+        exit 1
+else
+        exit 0
+fi
